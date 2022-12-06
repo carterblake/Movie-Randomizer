dd8ba984adf0ea8ee06860984432876c7ec089ca
angel Sanchez-Panadero <angelasp2001@gmail.com>
2022-12-05 21:04:58 -0600

    added event listeners to action button

--- assets/index.js
+++ assets/index.js
@@ -37,30 +37,39 @@ var tag = document.createElement('script');
   }
   function onPlayerStateChange(event) {
     changeBorderColor(event.data);
-  }
+  };
 
 
 
 
-  
+
 
   var omdb = 'http://www.omdbapi.com/?apikey=922eaf8d&t=';
-var randomMovie = randomize;
+  var actionButton = $("#action");
+  var ButtonHorror = $("#horror");
+  var ButtonMystery = $("#mystery");
+  var ButtonComedy = $("#comedy");
+  
+  var action = [ "bullet+train", "top++gun", "avatar", "Rogue+One%3A+A+Star+Wars+Story", "the+batman", "dune", "Spider-Man%3A+No+Way+Home", "the+dark+knight", "Inception", "Kantara " ];
+  var horror = ["Bhediya", "last+night+in+soho", "it", "the+shinning", "prey", "the+black+phone", "Pearl", "a+quiet+place", "bird+box", "train+to+busan"];
+  var mystery = ["knives+out", "sita+ramam", "se7en", "blade+runner+2049", "Prisoners", "shutter+island", "Zootopia", "gone+girl", "death+on+the+nile" ,"Enola+Holmes"];
+  var comedy = ["Amsterdam", "once+upon+a+time+in+Hollywood", "Everything+Everywhere+All+at+Once", "deadpool", "superbad", "We're+the+Millers", "the+mask", "21+jump+street", "bros", "the+hangover"];
+
+  //ar randomMovie = randomize();
 
 
-var action = [ "bullet+train", "top++gun", "avatar", "Rogue+One%3A+A+Star+Wars+Story", "the+batman", "dune", "Spider-Man%3A+No+Way+Home", "the+dark+knight", "Inception", "Kantara " ];
-var horror = ["Bhediya", "last+night+in+soho", "it", "the+shinning", "prey", "the+black+phone", "Pearl", "a+quiet+place", "bird+box", "train+to+busan"];
-var mystery = ["knives+out", "sita+ramam", "se7en", "blade+runner+2049", "Prisoners", "shutter+island", "Zootopia", "gone+girl", "death+on+the+nile" ,"Enola+Holmes"];
-var comedy = ["Amsterdam", "once+upon+a+time+in+Hollywood", "Everything+Everywhere+All+at+Once", "deadpool", "superbad", "We're+the+Millers", "the+mask", "21+jump+street", "bros", "the+hangover"];
-var movie = [""];
+  //click action button
+  //store one random action movie from action array
+  //run OMDB fetch promise
+  //fetch promise will append everything into page
 
 
 
-var randomize = action[i]
+actionButton.click(randomActionMovie);
 
-//console.log (randomize);
+function randomActionMovie() {
 
-
+  var randomMovie = action[Math.floor(math.random() * action.length)];
 
 fetch(omdb + randomMovie)
 .then(function (response) {
@@ -68,6 +77,9 @@ fetch(omdb + randomMovie)
 })
 .then(function (data) {
   console.log(data)
-  //Loop over the data to generate a table, each table row will have a link to the repo url
+ 
   
 });
+
+}
+
